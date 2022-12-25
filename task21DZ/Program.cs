8eea8d6a76780cbc678a6e92c6de5f1e3b7dc7b0
sabir gadzhimuradov <89633677773@mail.ru>
2022-12-25 17:48:31 +0300

    dz

--- task21DZ/Program.cs
+++ task21DZ/Program.cs
@@ -6,22 +6,28 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Введите число: ");
-string number = Console.ReadLine();
-int len = number.Length;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число: ");
+        string number = Console.ReadLine();
+        int len = number.Length;
 
-if (len == 5)
-{
-if (number[0] == number[4] && number[1] == number[3])
-{
-    Console.WriteLine($"{number} - Палиндром");
-}
-else
-{
-    Console.WriteLine($"{number} - Не палиндром");
-}
-}
-else
-{
-    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+        if (len == 5)
+        {
+            if (number[0] == number[4] && number[1] == number[3])
+            {
+                Console.WriteLine($"{number} - Палиндром");
+            }
+            else
+            {
+                Console.WriteLine($"{number} - Не палиндром");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+        }
+    }
 }
